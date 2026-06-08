baaac1a58211b9259b86cc985e89d897c8fb3c42
Gabriel <gabriellimma00@gmail.com>
2026-06-08 14:05:31 -0300

    feat: criacao de menu

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+void jogar(){}
+
+void tutorial(){}
+
+void sair(){}
 int main(void) {
-    printf("Hello, World!\n");
+
+
+    int opcao;
+
+    do {
+
+        printf("\n MENU \n");
+        printf("Jogar\n");
+        printf("Tutorial\n");
+        printf("Sair\n");
+
+        switch (opcao) {
+            case 1: jogar();
+                break;
+            case 2: tutorial();
+                break;
+            case 3: sair();
+                break;
+            default:
+                printf("\nOpcao invalida!\n");
+        }
+    }
+    while (opcao != 3);
     return 0;
+
 }
